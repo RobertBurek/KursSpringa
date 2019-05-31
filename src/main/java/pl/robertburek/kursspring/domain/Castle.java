6dfb3f7f4d69b807379642ad3923441255dc5bc5
Robert Burek <robertburek@wp.pl>
2019-05-31 21:11:34 +0200

    Czas życia Spring Beans
       @PostConstruct - metoda zostanie wykonana gdy spring beans bedzie
       tworzony, w momencie tworzenia kontenera, wszystkie metody tak oznaczone
       będą wykonane, nazwa metody nie ma znaczenia.
       @PreDestroy - metoda zostanie wykonana gdy spring zostanie zamknięty,
       wszystkie metody tak oznaczone będą wykonane, nazwa metody  nie ma znaczenia

--- src/main/java/pl/robertburek/kursspring/domain/Castle.java
+++ src/main/java/pl/robertburek/kursspring/domain/Castle.java
@@ -2,15 +2,31 @@ package pl.robertburek.kursspring.domain;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 /**
  * Created by Robert Burek
  */
 
-@Component  // spring beans
+@Component  // spring bean
 public class Castle {
     private String name = "East Watch";
 
     public Castle() {
+    }
 
+    @PostConstruct //metoda zostanie wykonana gdy spring bedzie tworzony
+    //w momencie tworzenia kontenera, wszystkie metody tak oznaczone będą wykonane
+    //nazwa metody nie ma znaczenia
+    public void build() {
+        System.out.println("Wybudowano zamek " + name);
+    }
+
+    @PreDestroy      //metoda zostanie wykonana gdy spring zostanie zamknięty
+    //wszystkie metody tak oznaczone będą wykonane
+    //nazwa metody  nie ma znaczenia
+    public void tearDown() {
+        System.out.println("Zaraz wyburzymy zamek " + name);
     }
 }
