7d50d6b43d1ddde768c768d01308bb6b58896b59
Robert Burek <robertburek@wp.pl>
2019-05-31 18:00:53 +0200

    Spring Beans
       do klasy dodajemy znacznik @Component

---
+++ src/main/java/pl/robertburek/kursspring/domain/Castle.java
@@ -0,0 +1,16 @@
+package pl.robertburek.kursspring.domain;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Robert Burek
+ */
+
+@Component  // spring beans
+public class Castle {
+    private String name = "East Watch";
+
+    public Castle() {
+
+    }
+}
