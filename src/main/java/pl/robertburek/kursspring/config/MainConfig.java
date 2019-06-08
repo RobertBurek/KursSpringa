437fbfafba91441e2e365a57c46f396b6730261b
Robert Burek <robertburek@wp.pl>
2019-06-08 21:53:21 +0200

    Wersja końcowa bez security    localhost:8080/h2-console/

--- src/main/java/pl/robertburek/kursspring/config/MainConfig.java
+++ src/main/java/pl/robertburek/kursspring/config/MainConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
 import pl.robertburek.kursspring.domain.repository.InDBKnightRepository;
 import pl.robertburek.kursspring.domain.repository.InMemoryKnightRepository;
 import pl.robertburek.kursspring.domain.repository.KnightRepository;
--- src/main/java/pl/robertburek/kursspring/config/SecurityConfig.java
+++ src/main/java/pl/robertburek/kursspring/config/SecurityConfig.java
@@ -2,9 +2,7 @@ package pl.robertburek.kursspring.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 import javax.sql.DataSource;
 
@@ -12,29 +10,29 @@ import javax.sql.DataSource;
  * Created by Robert Burek
  */
 
-@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    DataSource dataSource;
-
-    @Override
-    public void configure(HttpSecurity security) throws Exception {
-        security.authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()
-                .antMatchers("/knights").hasAnyAuthority("USER", "ADMIN")
-                .antMatchers("/knight").hasAnyAuthority("ADMIN")
-                .anyRequest().authenticated()
-                .and()//.httpBasic(); //tutaj też działa formularz logowania
-                .formLogin()   //bez tego też jest formularz logowania
-                .defaultSuccessUrl("/knights");
-    }
-
-    @Autowired
-    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception {
-        auth.jdbcAuthentication()
-                .dataSource(dataSource).usersByUsernameQuery(
-        "SELECT username,password,enabled FROM PLAYER_INFORMATION WHERE username = ?")
-                .authoritiesByUsernameQuery("SELECT username,userrole FROM ROLE WHERE username = ?");
-    }
-}
+//@Configuration
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Autowired
+//    DataSource dataSource;
+//
+//    @Override
+//    public void configure(HttpSecurity security) throws Exception {
+//        security.authorizeRequests()
+//                .antMatchers("/h2-console/**").permitAll()
+//                .antMatchers("/knights").hasAnyAuthority("USER", "ADMIN")
+//                .antMatchers("/knight").hasAnyAuthority("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()//.httpBasic(); //tutaj też działa formularz logowania
+//                .formLogin()   //bez tego też jest formularz logowania
+//                .defaultSuccessUrl("/knights");
+//    }
+//
+//    @Autowired
+//    public void securityUsers(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication()
+//                .dataSource(dataSource).usersByUsernameQuery(
+//        "SELECT username,password,enabled FROM PLAYER_INFORMATION WHERE username = ?")
+//                .authoritiesByUsernameQuery("SELECT username,userrole FROM ROLE WHERE username = ?");
+//    }
+//}
