e0687247172cfba5ac1997fb91066ba95d0bbc43
Robert Burek <robertburek@wp.pl>
2019-06-08 15:51:27 +0200

    Spring Security
       dodanie do projektu spring security poprzez dodanie dependencji
       oraz metody dodania użytkowników w pamięci, tylko że to nie działa.

--- src/main/java/pl/robertburek/kursspring/config/MainConfig.java
+++ src/main/java/pl/robertburek/kursspring/config/MainConfig.java
@@ -1,12 +1,16 @@
 package pl.robertburek.kursspring.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import pl.robertburek.kursspring.domain.repository.InDBKnightRepository;
 import pl.robertburek.kursspring.domain.repository.InMemoryKnightRepository;
 import pl.robertburek.kursspring.domain.repository.KnightRepository;
+
+import javax.annotation.Resource;
 
 /**
  * Created by Robert Burek
@@ -27,5 +31,15 @@ public class MainConfig {
     public KnightRepository createInDBRepo() {
         KnightRepository repo = new InDBKnightRepository();
         return repo;
+    }
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception{
+        //to jest rozwiązanie developerski a nie produkcyjne (generowanie w pamięci) zreszta nie działa
+//        authenticationManagerBuilder.inMemoryAuthentication()
+//                .withUser("user1").password("user1").roles("USER").and()
+//                .withUser("Robert").password("Robert7").roles("ADMIN").and()
+//                .withUser("InnyUser").password("InnyUser7").roles("USER");
+        System.out.println("---------------ustawienie użytkowników nie działa !!!--------------------------------");
     }
 }
